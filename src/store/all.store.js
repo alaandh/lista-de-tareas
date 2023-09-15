0e5086cdd6d8fa53997500289f52e9a07f0d1f68
Alan Heizenreder <alanheizenreder01@gmail.com>
2023-09-15 17:02:12 -0300

    method implementation

--- src/store/all.store.js
+++ src/store/all.store.js
@@ -12,6 +12,8 @@ const state = {
     new All("Piedra del infinito"),
     new All("Piedra del tiempo"),
     new All("Piedra del espacio"),
+    new All("Piedra del poder"),
+    new All("Piedra del realidad"),
   ],
   filter: Filters.Lot,
 };
@@ -25,19 +27,63 @@ const loadStore = () => {
   throw new Error("Not implemented");
 };
 
+const getAll = (filter = Filters.All) => {
+  switch (filter) {
+    case Filters.All:
+      return [...state.alls];
+
+    case Filters.Completed:
+      return state.alls.filter((all) => all.done);
+
+    case Filters.Pending:
+      return state.alls.filter((all) => !all.done);
+
+    default:
+      throw new Error(`Option ${filter} is not valid`);
+  }
+};
+
 const addAll = (description) => {
-  throw new Error("Not implemented");
+  if (!description) {
+    throw new Error("Description is required");
+  }
+
+  state.alls.push(new All(description));
 };
 
 const toggleAll = (allId) => {
-  throw new Error("Not implemented");
+  state.alls = state.alls.map((all) => {
+    if (all.id === allId) {
+      all.done = !all.done;
+    }
+    return all;
+  });
 };
 
 const deleteAll = (allId) => {
-  throw new Error("Not implemented");
+  state.alls = state.alls.filter((all) => all.id !== allId);
+};
+
+const deleteCompleted = () => {
+  state.alls = state.alls.filter((all) => all.done);
+};
+
+const setFilter = (newFilter = Filters.All) => {
+  state.filter = newFilter;
+};
+
+const getCurrentFilter = () => {
+  return state.filter;
 };
 
 export default {
+  getAll,
+  addAll,
   initStore,
   loadStore,
+  toggleAll,
+  deleteAll,
+  setFilter,
+  deleteCompleted,
+  getCurrentFilter,
 };
